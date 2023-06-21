bb7a1540682d11354ad248800f90304485514225
ahsanrbaloch <ahsanrasheed135@gmail.com>
2023-06-21 12:16:09 +0500

    Routing

--- src/App.js
+++ src/App.js
@@ -6,20 +6,22 @@ import NewPost from './NewPost';
 import PostPage from './PostPage'
 import About from './About'
 import Missing from './Missing'
-import {Route, Routes, useHistory} from 'react-router-dom';
+import {Route, BrowserRouter as Router, Routes, useHistory} from 'react-router-dom';
 import { useState, useEffect } from 'react';
 function App() {
   return (
     <div className="App">
           <Header/>
           <Nav />
-          <Routes>
-              <Route path="/" element={<Home />}/>
-              <Route path="/posts" element={<NewPost />}/>
-              <Route path="/posts/:id" element={<PostPage />}/>
-              <Route path="/about" Component={About} />
-              <Route path="/*" Component={Missing} />
-          </Routes>
+              <Routes>
+                      <Route exact path="/" element={<Home />}/>
+                      <Route exact path="/post" element={<NewPost />}/>
+                      <Route path="/post/:id" element={<PostPage />}/>
+                      <Route path="/about" Component={About} />
+                      <Route path="*" Component={Missing} />
+                  </Routes>
+  
+          
           <Footer />
     </div>
   );
