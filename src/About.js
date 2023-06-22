f2cb75e4329ef7a7501f38f768a965def31cae0f
ahsanrbaloch <ahsanrasheed135@gmail.com>
2023-06-22 19:26:57 +0500

    application

--- src/About.js
+++ src/About.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <main>
-     <h1>About</h1> 
+    <main className='About'>
+     <h2>About</h2>
+     <p style={{marginTop:"1rem"}}>This blog app is one of the projects in the React Js Learning Practices. </p> 
     </main>
   )
-}
+} 
 
 export default About
