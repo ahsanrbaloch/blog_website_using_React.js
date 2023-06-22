765177fb1d27d1c137de2039d66d2f00a318f711
ahsanrbaloch <ahsanrasheed135@gmail.com>
2023-06-22 19:28:10 +0500

    application

--- src/App.js
+++ src/App.js
@@ -6,22 +6,90 @@ import NewPost from './NewPost';
 import PostPage from './PostPage'
 import About from './About'
 import Missing from './Missing'
-import {Route, Routes, useHistory} from 'react-router-dom';
+import { format } from 'date-fns';
+import {Route, Routes, useNavigate} from 'react-router-dom';
 import { useState, useEffect } from 'react';
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "My First Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 2,
+      title: "My 2nd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 3,
+      title: "My 3rd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 4,
+      title: "My Fourth Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    }
+  ])
+  const [search, setSearch]=useState('');
+  const [searchResults, setSearchResults]=useState([]);
+  const [postTitle, setPostTitle]=useState('');
+  const [postBody, setPostBody]=useState('');
+  const navigate=useNavigate();
+  useEffect(() =>{
+    const filteredResult=posts.filter(post=> 
+      ((post.body).toLowerCase()).includes(search.toLowerCase())
+      || ((post.title).toLowerCase()).includes(search.toLowerCase()));
+      setSearchResults(filteredResult.reverse());
+  },[posts, search])
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log();
+    const id=posts.length ? posts[posts.length-1].id +1 : 1;
+    const datetime=format(new Date(), 'MMMM dd, yyyy pp');
+    const newPost={id, title: postTitle, datetime, body: postBody};
+    const allPosts=[...posts,newPost];
+    setPosts(allPosts);
+    setPostTitle('');
+    setPostBody('');
+    navigate('/');
+
+
+  }
+  
+  const handleDelete=(id)=>
+  {
+    const newPosts=posts.filter(post => post.id!==id);
+    setPosts(newPosts);
+    navigate('/');
+  }
   return (
     <div className="App">
-          <Header/>
-          <Nav />
+          <Header title="React JS Blog"/>
+          <Nav search={search} setSearch={setSearch}/>
               <Routes>
-                      <Route exact path="/" element={<Home />}/>
-                      <Route exact path="/post" element={<NewPost />}/>
-                      <Route path="/post/:id" element={<PostPage />}/>
+                      <Route exact path="/" element={<Home posts={searchResults}/>}/>
+                      <Route exact path="/post" element={<NewPost 
+                              postTitle={postTitle}
+                              setPostTitle={setPostTitle}
+                              postBody={postBody}
+                              setPostBody={setPostBody}
+                              handleSubmit={handleSubmit}
+                              />
+                        }/>
+                      <Route path="/post/:id" element={<PostPage 
+                              posts={posts} 
+                              handleDelete={handleDelete}
+                              />
+                          }/>
                       <Route path="/about" Component={About} />
                       <Route path="*" Component={Missing} />
                   </Routes>
-  
-          
           <Footer />
     </div>
   );
